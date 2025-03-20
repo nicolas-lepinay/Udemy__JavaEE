5b53142b0161270ad6a68f5e128891afa0b7a0ef
Nicolas Lépinay <nicolas@nosoft.io>
2025-03-20 08:36:18 +0100

    Section 11.63

---
+++ webapp/src/main/java/com/mycompany/tennis/webapp/controller/ListJoueursServlet.java
@@ -0,0 +1,34 @@
+package com.mycompany.tennis.webapp.controller;
+
+import com.mycompany.tennis.core.dto.JoueurDto;
+import com.mycompany.tennis.core.service.JoueurService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/listes")
+public class ListJoueursServlet extends HttpServlet {
+
+    private JoueurService joueurService;
+
+    public ListJoueursServlet() {
+        this.joueurService = new JoueurService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<JoueurDto> listeHommes = joueurService.getListeJoueurs('H');
+        List<JoueurDto> listeFemmes = joueurService.getListeJoueurs('F');
+
+        req.setAttribute("listeHommes", listeHommes);
+        req.setAttribute("listeFemmes", listeFemmes);
+        RequestDispatcher disp = req.getRequestDispatcher("listes.jsp"); // URL
+        disp.forward(req, resp);
+    }
+}
---
+++ webapp/src/main/webapp/listes.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Listes des joueurs</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+</body>
+</html>
